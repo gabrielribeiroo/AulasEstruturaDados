a28927b53467d6f73ff469bdd5c626e53b9a5e8d
gabriel.rvital <gabriel.rvital@CAS555487W1029.senacsp.edu.br>
2019-09-25 21:59:19 -0300

    Exercicio 3 conversão decimal para binário

--- src/Aula_08/Ex_03.java
+++ src/Aula_08/Ex_03.java
@@ -16,12 +16,26 @@ public class Ex_03 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Scanner leitor = new Scanner (System.in);
+    public static void main(String[] args) throws Exception {
+        Scanner leitor = new Scanner(System.in);
         System.out.println("Digite o número decimal para conversão: ");
         int dec = leitor.nextInt();
-        
-        
+        String bin = dec2Bin(dec);
+        System.out.println("Valor em Binário: " + bin);
+
     }
-    
+
+    public static String dec2Bin(int dec) throws Exception {
+        Pilha p = new Pilha();
+        String bin = "";
+        while (dec > 0) {
+            p.empilha(dec % 2);
+            dec = dec / 2;
+        }
+        while (!p.vazia()) {
+            bin += p.desempilha();
+        }
+        return bin;
+    }
+
 }
